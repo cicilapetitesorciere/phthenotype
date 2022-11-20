6020055b86692929cd6e794bce58ee1408355d22
cicilapetitesorciere <cici@Yamazakura.Home>
2022-11-20 13:43:32 -0500

    fixed install script in light of new gitignore

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ INSTALL_PATH=~/.local/bin/
 
 #!([ -f install.sh ] && [ -f ptheno.sh]) && echo "Error: Please go to the directory that has the install script before installing" && exit
 
-(echo "#/usr/bin/env bash" && echo PTHENODIR=$(pwd) && cat ptheno.sh) > $INSTALL_PATH/ptheno
+mkdir -p active
+
+(echo \#\!/bin/sh && echo PTHENODIR=$(pwd) && cat ptheno.sh) > $INSTALL_PATH/ptheno
 chmod +x $INSTALL_PATH/ptheno
 
 #echo "Success!"
